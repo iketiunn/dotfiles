b7109bb54a170321f4499ca1723ea3ef2730bf82
ike.chang <ike.chang@sideeffect.dev>
2024-01-09 16:30:37 +0800

    feat: using vscode theme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -191,10 +191,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'Mofiqul/vscode.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 
@@ -205,7 +205,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'vscode',
         component_separators = '|',
         section_separators = '',
       },
