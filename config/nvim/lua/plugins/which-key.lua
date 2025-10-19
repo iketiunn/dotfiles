785c0a3099d40347fd514b693da61976b8079964
ike.chang <ike.chang@sideeffect.dev>
2025-10-19 15:34:36 +0800

    chore(nvim): move leader key based keymaps into which key

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,8 +1,34 @@
-return {  
-  {  
-    "folke/which-key.nvim",  
-    opts = {  
-      preset = "modern",  
-    },  
-  },  
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "modern",
+      spec = {
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>y", group = "[Y]ank" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+
+      -- Toggle commands
+      wk.add({
+        { "<leader>th", ":set hlsearch!<CR>", desc = "[T]oggle [H]ighlight", icon = "🔍" },
+      })
+      -- Yank commands
+      wk.add({
+        { "<leader>yp", function()
+            local relative_path = vim.fn.expand('%:.')
+            vim.fn.setreg('+', relative_path)
+            vim.notify('Yanked relative path: ' .. relative_path)
+          end, desc = "Yank relative file path", icon = "📄" },
+        { "<leader>yP", function()
+            local absolute_path = vim.fn.expand('%:p')
+            vim.fn.setreg('+', absolute_path)
+            vim.notify('Yanked absolute path: ' .. absolute_path)
+          end, desc = "Yank absolute file path", icon = "📁" },
+      })
+    end,
+  },
 }
